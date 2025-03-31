cea58b665496bb100eccd0bbd68436fff0d8cad3
AnonymusAng <joan.erazo.z@uniautonoma.edu.co>
2025-03-31 12:38:06 -0500

    Creo Nuevo Archivo

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+def suma(numA,numB):
+    total=numA+numB
+    print("La suma de los dos numeros es: ",total)
+    return total
+
+numA=int(input("Por favor ingrese el primer numero: "))
+numB=int(input("Por favor ingrese el segundo numero: "))
+suma(numA,numB)
+
+
