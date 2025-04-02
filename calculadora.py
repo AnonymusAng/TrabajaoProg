b36aaaba542f261d104b33ed59e9d65fc02b77ba
AnonymusAng <joan.erazo.z@uniautonoma.edu.co>
2025-04-02 11:35:07 -0500

    Agrego Funcion Resta

--- calculadora.py
+++ calculadora.py
@@ -3,8 +3,15 @@ def suma(numA,numB):
     print("La suma de los dos numeros es: ",total)
     return total
 
+def resta(a, b):  
+    total=numA-numB
+    print("La resta de los dos numeros es: ",total)
+    return total
+
 numA=int(input("Por favor ingrese el primer numero: "))
 numB=int(input("Por favor ingrese el segundo numero: "))
 suma(numA,numB)
+resta(numA,numB)
+
 
 
